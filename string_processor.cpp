19493fe6a8a8ab49e05e23cd7bee086154adccb3
Egor Zheliba <92647034+JessiePinkMann@users.noreply.github.com>
2024-11-01 02:48:59 +0300

    Update string_processor.cpp

--- string_processor.cpp
+++ string_processor.cpp
@@ -6,7 +6,7 @@
 void processString(const char* input) {
     char buffer[10];
 
-    if (strlen(input) < 10) {
+    if (strlen(input) <= 10) {
         strcpy(buffer, input); // Возможная уязвимость к переполнению буфера
         std::cout << "Input processed: " << buffer << std::endl;
     } else {
