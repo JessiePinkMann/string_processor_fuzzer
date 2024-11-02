13c73966f49718f473739cdcd95e200df1982d47
Egor Zheliba <92647034+JessiePinkMann@users.noreply.github.com>
2024-11-02 14:51:44 +0300

    Update string_processor.cpp

--- string_processor.cpp
+++ string_processor.cpp
@@ -7,7 +7,7 @@ void processString(const char* input) {
     char buffer[10];
 
     if (strlen(input) <= 10) {
-        strcpy(buffer, input); // Возможная уязвимость к переполнению буфера
+        strcpy(buffer, input);
         std::cout << "Input processed: " << buffer << std::endl;
     } else {
         std::cout << "Input too long!" << std::endl;
