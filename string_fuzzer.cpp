1f9f77642bd14f6fe9f5cbd38f37b0d66e5e3d1c
Egor Zheliba <92647034+JessiePinkMann@users.noreply.github.com>
2024-11-02 14:52:10 +0300

    Update string_fuzzer.cpp

--- string_fuzzer.cpp
+++ string_fuzzer.cpp
@@ -2,10 +2,8 @@
 #include <string>
 #include "string_processor.h"
 
-// Функция, которая будет использоваться для фаззинга
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     if (size > 0) {
-        // Конвертируем данные в строку
         std::string input(reinterpret_cast<const char*>(data), size);
         processString(input.c_str());
     }
